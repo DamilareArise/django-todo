6cc27d9ce8a0cac21294285919f5060870e5047d
Arise Damilare <arisedamilare5@gmail.com>
2025-02-08 12:33:56 +0100

    initialize Django Todo application with models, views, and templates

---
+++ todo/urls.py
@@ -0,0 +1,48 @@
+"""
+URL configuration for todo project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.contrib import admin
+from django.urls import path
+from django.views.generic import TemplateView
+from todoManager.views import addTodo
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", TemplateView.as_view(template_name = "index.html"), name = 'home'),
+    path("get-todo-page/", TemplateView.as_view(template_name = "addTodoPAge.html"), name = 'get-todo-page'),
+    path("add-todo", addTodo, name = 'add-todo'),
+]
+
+
+
+# model, (ORM -> object relational mapping) 
+# view -> functionality of the app, 
+# urls -> (uniform resource locator), 
+'''
+what makes up a url is 
+1. path
+2. view (functions)
+3. name
+
+'''
+# forms -> , 
+# template, -> template 
+
+def add():
+    print("Adding a new item")
+    
+add()
---
+++ todoManager/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, redirect
+from .models import Todo
+
+# Create your views here.
+
+# CRUD => Create, Read, Update, Delete
+
+def addTodo(request):
+    if request.method == 'POST':
+        # print(request.POST)
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        print(title, description)
+        
+        Todo.objects.create(
+            title = title,
+            description = description
+        )
+        
+        return redirect('home')
+    
+    return render(request, template_name='addTodopage.html')
