5c3e8859fbfffc4e4c8105ba9f93313ea3a97198
Arise Damilare <arisedamilare5@gmail.com>
2025-03-15 11:56:09 +0100

    add profile page template and user profile model with migrations for profile picture

--- userApp/forms.py
+++ userApp/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 
 class SignUpForm(UserCreationForm):
@@ -15,3 +16,22 @@ class SignUpForm(UserCreationForm):
             'password2'
         ]
 
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = [
+            'username',
+            'first_name',
+            'last_name',
+            'email',
+        ]
+        
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = [
+            'phone_number',
+            'address',
+            'gender',
+            'profile_picture'
+        ]
--- userApp/views.py
+++ userApp/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from .forms import SignUpForm
 from django.views import generic
 from django.urls import reverse_lazy
+from django.contrib.auth.models import User
+from .models import Profile
 
 
 # Create your views here.
@@ -11,3 +13,11 @@ class SignUpView(generic.CreateView):
     success_url  = reverse_lazy("login")
     template_name = 'registration/signup.html'
     
+    
+def ProfileView(request):
+    userInfo = User.objects.get(id = request.user.id)
+    profileInfo = Profile.objects.get(user_id=request.user.id)
+    
+    print(f'picture: {profileInfo.profile_picture}')
+    
+    return render(request, 'profile.html', {'userInfo': userInfo, 'profileInfo': profileInfo})
